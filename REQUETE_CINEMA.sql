27176f64802e0663c72304dbee427c8e7be3b72c
Asma <148782301+yah422@users.noreply.github.com>
2024-03-12 16:54:37 +0100

    Update REQUETE_CINEMA.sql

--- REQUETE_CINEMA.sql
+++ REQUETE_CINEMA.sql
@@ -4,3 +4,39 @@ SELECT film.titre_film, film.anneeSortie_film, film.duree_film, realisateur.id_r
 FROM film
 INNER JOIN realisateur  ON film.id_film = realisateur.id_realisateur
 
+b. Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)
+
+
+  
+c. Liste des films d’un réalisateur (en précisant l’année de sortie) 
+
+
+  
+d. Nombre de films par genre (classés dans l’ordre décroissant)
+
+  
+e. Nombre de films par réalisateur (classés dans l’ordre décroissant)
+
+
+  
+f. Casting d’un film en particulier (id_film) : nom, prénom des acteurs + sexe
+
+
+  
+g. Films tournés par un acteur en particulier (id_acteur) avec leur rôle et l’année de sortie (du film le plus récent au plus ancien)
+
+  
+h. Liste des personnes qui sont à la fois acteurs et réalisateurs
+
+  
+i. Liste des films qui ont moins de 5 ans (classés du plus récent au plus ancien)
+
+  
+j. Nombre d’hommes et de femmes parmi les acteurs
+
+  
+k. Liste des acteurs ayant plus de 50 ans (âge révolu et non révolu)
+
+  
+l. Acteurs ayant joué dans 3 films ou plus
+
