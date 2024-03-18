8a28bba4397b01e8ddbcdc9e30e02a50175d7a5b
Asma SAIDI <saidiasma422@gmail.com>
2024-03-18 16:10:26 +0100

    add requete for the connection with the database

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -4,7 +4,26 @@
 namespace Controller;
 use Model\Connect;
 
-class CinemaController {
+class FilmsController {
+// --------------- LISTER LES FILMS ---------------
+    public function listFilms(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo ->query("
+            SELECT
+            id_film,
+            title_film,
+            anneeSortie_film,
+            synopsis_film,
+            note_film,
+            DATE_FORMAT(film.duree_film, '%H:%i') AS duree_formatee, 
+            affiche_film, 
+            id_realisateur
+            FROM Film 
+            INNER JOIN realisateur ON realisateur.id_realisateur = movie.id_realisateur
+            INNER JOIN personne ON personne.id_personne = director.id_personne
+        ");
 
-    
+    }
+
+
 }
