756739df677a38ef771044edbff85ff8a3576468
Asma SAIDI <saidiasma422@gmail.com>
2024-03-18 16:50:34 +0100

    add listfilm file with loop and variables

--- view/listFilms.php
+++ view/listFilms.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des films </title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php ob_start(); ?>
+
+<p class="uk-label uk-label-warning"> Il y a <?= $requete ->rowCount() ?> </p>
+
+<table class="uk-table uk-table-striped">
+    <thead>
+        <tr>
+            <th> TITRE </th>
+            <th> ANNEE SORTIE </th>
+            <th> SYNONPSIS </th>
+            <th> AFFICHE FILM </th>
+            <th> NOTE FILM </th>
+            <th> DUREE FILM </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach($requete ->fetchAll() as $film) {?>
+                <tr>
+                    <td> <?= $film["titre"]?> </td>
+                    <td> <?= $film["annee_sortie"]?> </td>
+                </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+$titre = "Liste des films";
+$titre_secondaire = "Liste des films";
+$contenu = ob_get_clean();
+require "view/template.php";
+
+?>
