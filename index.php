278152703764f4ddb50882a45741651a1c93a23e
Asma SAIDI <saidiasma422@gmail.com>
2024-03-18 15:52:19 +0100

    add use keyword for all the variable sand the controller too

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <?php
-// -------------  il indique à une classe d hériter d un trait et il donne un alias à un espace de noms.use ------------
 
-use Controller\CinemaController;
+// -------------  il indique à une classe d hériter d un trait et il donne un alias à un espace de noms.use ------------
+use Controller\FilmsController;
+use Controller\ActeursController;
+use Controller\RealisateursController;
+use Controller\CategoriesController; 
+use Controller\RolesController;
+
+// ----------------- AUTO CHARGE LES CLASSES DU PROJET ---------------------
 spl_autoload_register(function($class_name){
     include $class_name . '.php';
 });
 
-// ---------------- MISE EN PLACE DE LA VARIABLE ---------------
-$ctrlCinema = new CinemaController();
+// On instancie le controller Cinema
+$ctrlFilms = new FilmsController();
+$ctrlActeurs = new ActeursController();
+$ctrlRealisateurs = new RealisateursController();
+$ctrlCategories = new CategoriesController();
+$ctrlRoles = new RolesController();
+
+// En fonction de l'action détectée dans l'URL par "action" on connecte avec la bonne méthode du controller
+
+$id=(isset($_GET["id"])) ? $_GET["id"] : null;
+// Format -> $type = (isset($_GET["type])) ? $_GET["type"] : null;
 
 if(isset($_GET["action"])){
-    
-// ----------------- AUTO CHARGE LES CLASSES DU PROJET ---------------------
+
     switch ($_GET["action"]){
         case "listFilms" : $ctrlCinema -> listFilms(); break;
         case "listActeurs" : $ctrlCinema -> listActeurs(); break;
+        case "listCategories" : $ctrlCinema -> listCategories(); break;
+
 
     }
 }
