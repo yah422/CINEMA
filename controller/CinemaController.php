bbe99896ab819569aa9fa5b93c90e548503f29df
Asma SAIDI <saidiasma422@gmail.com>
2024-03-18 16:15:13 +0100

    add other infos from real and personne table

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -10,14 +10,16 @@ class FilmsController {
         $pdo = Connect::seConnecter();
         $requete = $pdo ->query("
             SELECT
-            id_film,
-            title_film,
-            anneeSortie_film,
-            synopsis_film,
-            note_film,
+            film.id_film,
+            film.title_film,
+            film.anneeSortie_film,
+            film.synopsis_film,
+            film.note_film,
             DATE_FORMAT(film.duree_film, '%H:%i') AS duree_formatee, 
-            affiche_film, 
-            id_realisateur
+            film.affiche_film, 
+            realisateur.id_realisateur,
+            personne.nom_personne,
+            personne.prenom_personne
             FROM Film 
             INNER JOIN realisateur ON realisateur.id_realisateur = movie.id_realisateur
             INNER JOIN personne ON personne.id_personne = director.id_personne
