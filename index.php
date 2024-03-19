4e0f9f0e45175b4dc6bc0420bd813c7a1248fe22
Asma SAIDI <saidiasma422@gmail.com>
2024-03-19 14:10:48 +0100

    add colorful comment to the index.php

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
-// !-------------  il indique à une classe d hériter d un trait et il donne un alias à un espace de noms.use ------------
+// &-------------  il indique à une classe d hériter d un trait et il donne un alias à un espace de noms.use ------------
 use Controller\FilmController;
 use Controller\ActeurController;
 use Controller\RealisateurController;
 use Controller\CategorieController; 
 use Controller\RoleController;
 
-// !----------------- AUTO CHARGE LES CLASSES DU PROJET ---------------------
+// &----------------- AUTO CHARGE LES CLASSES DU PROJET ---------------------
 spl_autoload_register(function($class_name){
     include $class_name . '.php';
 });
 
-// !On instancie le controller Cinema
+// &On instancie le controller Cinema
 $ctrlFilm = new FilmController();
 $ctrlActeur = new ActeurController();
 $ctrlRealisateur = new RealisateurController();
 $ctrlCategorie = new CategorieController();
 $ctrlRole = new RoleController();
 
-// !En fonction de l'action détectée dans l'URL par "action" on connecte avec la bonne méthode du controller
+// &En fonction de l'action détectée dans l'URL par "action" on connecte avec la bonne méthode du controller
 
 $id=(isset($_GET["id"])) ? $_GET["id"] : null;
-// !Format -> $type = (isset($_GET["type])) ? $_GET["type"] : null;
+// &Format -> $type = (isset($_GET["type])) ? $_GET["type"] : null;
 
 if(isset($_GET["action"])){
 
