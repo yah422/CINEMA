605087708e9e8dc7d6595be19ad7ef111556d4be
Asma SAIDI <saidiasma422@gmail.com>
2024-03-20 17:00:12 +0100

    add indentations and comment to the categorie part

--- controller/CategorieController.php
+++ controller/CategorieController.php
@@ -6,30 +6,30 @@ namespace Controller;
 use Model\Connect;
 
 class CategorieController {
-// ^^--------------- LISTER LES ACTEURS ---------------
-    public function listCategorie(){
+  // ^^--------------- LISTER LES ACTEURS ---------------
+  public function listCategorie(){
 
-        // ^^On se connecte
-        $pdo= Connect::seConnecter();
-
-         // ^^On exécute la requête de notre choix
-         $requete = $pdo -> query("
-         SELECT
-         nom_genreCine
-         FROM genrecine;
-         
-        ");
-    
-     // ^^On relie par un "require" la vue qui nous intéresse
-     require "view/categorie/listCategories.php";
-   }
-
-   //  ^^ ------------- DETAILS CATEGORIES --------------
-
-   public function detailCategorie($id){
-
+    // ^^On se connecte
     $pdo= Connect::seConnecter();
 
+      // ^^On exécute la requête de notre choix
+      $requete = $pdo -> query("SELECT
+      nom_genreCine
+      FROM genrecine;
+      ");
+
+    // ^^On relie par un "require" la vue qui nous intéresse
+    require "view/categorie/listCategories.php";
+  }
+
+  //  ^^ ------------- DETAILS CATEGORIES --------------
+
+  public function detailCategorie($id){
+    
+    // ^^On se connecte
+    $pdo= Connect::seConnecter();
+
+    // ^^On exécute la requête de notre choix
     $requete = $pdo -> prepare("SELECT * FROM genreCine WHERE id_genreCine = :id");
     $requete -> execute(["id"=> $id]);
     
@@ -38,7 +38,8 @@ class CategorieController {
     nom_genreCine
     FROM genreCine;");
 
+    // ^^On relie par un "require" la vue qui nous intéresse
     require "view/categorie/detailCategorie.php";
 
-   }
+  }
 }
