2c0bdd0624dc449bc17a9388b1f848ea8b821125
Asma SAIDI <saidiasma422@gmail.com>
2024-03-22 17:00:07 +0100

    add all different input for the AjoutFilm part

--- view/film/ajoutFilm.php
+++ view/film/ajoutFilm.php
@@ -5,12 +5,57 @@ ob_start();
 
 
 <form action="index.php?action=ajoutFilm" method="post" enctype="multipart/form-data">
-    <p class="px-2">
+    <p  class="px-2">
         <label>
-            Nom du film : <br>
+            Titre du film : <br>
             <input id="" class="border-1" name="titre_film" type="text">
+            <br>
         </label>
     </p>
+    <p class="px-2">
+        <label>
+            Durée du film : <br>
+            <input id="" class="border-1" name="duree_formatee" type="text">
+            
+        </label>
+    </p>
+    <p class="px-2">
+        <label>
+            Année de parution : <br>
+            <input id="" class="border-1" name="anneeSortie_film" type="text">
+           
+        </label>
+    </p>
+    <p class="px-2">
+        <label>
+            Synopsis : <br>
+            <input id="" class="border-1" name="synopsis_film" type="text">
+            
+        </label>
+    </p>
+    <p class="px-2">
+        <label>
+            Réalisateur: <br>
+            <input id="" class="border-1" name="realisateurName" type="text">
+           
+        </label>
+    </p>
+    <p class="px-2">
+        <label>
+            Note: <br>
+            <input id="" class="border-1" name="note_film" type="text">
+           
+        </label>
+    </p>
+    <p class="px-2">
+        <label>
+            Affiche du film: <br>
+            <input id="" class="border-1" name="affiche_film" type="text">
+            
+        </label>
+    </p>
+
+    
 
 
 
