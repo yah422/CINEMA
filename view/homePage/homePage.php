328e8792f9b09d7b6ed52ac5047f97e2e89768b4
SAIDI Asma <saidiasma422@gmail.com>
2024-03-27 11:30:35 +0100

    add link font in the new css file

--- view/homePage/homePage.php
+++ view/homePage/homePage.php
@@ -7,18 +7,9 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <a href="index.php?action=homePage">
-            <div class="logo">
-                <img src="./public/Images/Movie Maker.png" alt="logo CinemaAsma">
-            </div>
-        </a>
-
-    </header>
-
+  
     <main>
 
     </main>
--- view/template.php
+++ view/template.php
@@ -4,26 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title> <?= $titre ?> </title>
 </head>
 <body>
     <header>
-        <nav style="display: flex;
-        flex-direction: row;
-        align-content: center;
-        justify-content: center;
-        align-items: center;" class="navbar bg-primary" data-bs-theme="dark">
-            <ul style="display: flex;
-            flex-direction: row;
-            gap: 40px;
-            list-style: none;
-            color: white;">
-                <li><a style="text-decoration: none;" href="index.php?action=homePage"> HOME </a></li>
-                <li><a style="text-decoration: none;" href="index.php?action=listFilm"> FILMS </a></li>
-                <li><a style="text-decoration: none;" href="index.php?action=listActeur"> ACTEURS </a></li>
-                <li><a style="text-decoration: none;" href="index.php?action=listRealisateur"> REALISATEURS </a></li>
-                <li><a style="text-decoration: none;" href="index.php?action=listRole">ROLES</a>  </li>
-                <li> <a style="text-decoration: none;" href="index.php?action=listCategorie"> CATEGORIE </a></li>
+
+        
+
+        <nav>
+            <a href="index.php?action=homePage">
+                <div class="logo">
+                    <img src="./public/images/Movie Maker.png" alt="logo CinemaAsma">
+                </div>
+            </a>
+            <ul>
+                <li><a href="index.php?action=homePage"> HOME </a></li>
+                <li><a href="index.php?action=listFilm"> FILMS </a></li>
+                <li><a href="index.php?action=listActeur"> ACTEURS </a></li>
+                <li><a href="index.php?action=listRealisateur"> REALISATEURS </a></li>
+                <li><a href="index.php?action=listRole">ROLES</a>  </li>
+                <li><a href="index.php?action=listCategorie"> CATEGORIE </a></li>
             </ul>
         </nav>
     </header>
