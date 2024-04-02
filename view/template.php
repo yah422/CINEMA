9811b5458137809ed8c4510663978ff12cdb801c
Asma <saidiasma422@gmail.com>
2024-04-02 22:46:23 +0200

    add style to the contenu in the template file

--- view/template.php
+++ view/template.php
@@ -35,7 +35,7 @@
 
     </header>
 
-        <div id="contenu"> 
+        <div id="contenu" style="display: flex; flex-direction: column; justify-content: center; align-items: center;"> 
             <h1 class="display-1"> Cinéma </h1>
             <h2 class="h2" > <?= $titre_secondaire ?></h2>
         </div>
