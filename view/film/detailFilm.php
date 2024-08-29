1f1202a8d7cddbe09da45744794495c7b7c7dd1c
Asma <saidiasma422@gmail.com>
2024-08-29 22:43:57 +0200

    change the css to add a root part with color code and correct the html

--- view/film/detailFilm.php
+++ view/film/detailFilm.php
@@ -6,93 +6,82 @@ $film = $requeteFilm->fetch();
 
 // Récupération du synopsis du film
 $syno = $requeteFilmS->fetch();
-
 ?>
 
 <section>
-    
     <div id="wrapper">
         <div>
-            <img src='<?= $film["affiche_film"] ?>' alt='Affiche du film' style="width:250px;">
+            <img src='<?= $film["affiche_film"] ?>' alt='Affiche du film'>
         </div>
 
         <div>
-            <p> Titre Film : <?= $film["titre_film"] ?></p>
-            <p> Durée : <?= $film["duree_formatee"] ?></p>
-            <p> Année de parution : <?= $film["anneeSortie_film"] ?></p>
-            <?php
-                if ($requeteG->fetch()) {
-                    ?>
-                    <a href="index.php?action=detailCategorie&id=<?= $requeteG->fetch()["id_genreCine"]?>"><p> Catégorie : <?= $genre["nom_genreCine"] ?></p></a>
-                    <?php
-                }
-           
-                ?>
-            <a href="index.php?action=detailRealisateur&id=<?= $film["id_realisateur"] ?>"><p> Réalisateur : <?= $film["realisateurName"] ?> </p></a>
+            <p>Titre Film : <?= $film["titre_film"] ?></p>
+            <p>Durée : <?= $film["duree_formatee"] ?></p>
+            <p>Année de parution : <?= $film["anneeSortie_film"] ?></p>
 
-            <br>
+            <?php if ($requeteG->fetch()) { ?>
+                <a href="index.php?action=detailCategorie&id=<?= $requeteG->fetch()["id_genreCine"] ?>">
+                    <p>Catégorie : <?= $genre["nom_genreCine"] ?></p>
+                </a>
+            <?php } ?>
 
-            <div id="note"> Note : <?= $film["note_film"] ?></div>
+            <a href="index.php?action=detailRealisateur&id=<?= $film["id_realisateur"] ?>">
+                <p>Réalisateur : <?= $film["realisateurName"] ?></p>
+            </a>
+
+            <div id="note">Note : <?= $film["note_film"] ?></div>
         </div> 
-       
     </div>
-    
-
-    <br>
-    <br>
 
     <div id="wrap">
-
         <div class="parts">
-            <div class="ligneAcceuil"> </div>
-            <h2> SYNOPSIS <br> </h2>
+            <div class="ligneAcceuil"></div>
+            <h2>SYNOPSIS</h2>
         </div>
 
         <div class="title">
-            <p style="text-align:justify;"> <?= $syno["synopsis_film"] ?></p>
+            <p><?= $syno["synopsis_film"] ?></p>
         </div>
-        <br>
-
 
         <div class="parts">
-            <div class="ligneAcceuil"> </div>
-            <h2> CASTING <br> </h2>
+            <div class="ligneAcceuil"></div>
+            <h2>CASTING</h2>
         </div>
 
-        <br>
         <div class="title">
-            <table class="table-responsive" style="width: 600px;">
+            <table class="table-responsive">
                 <thead>
                     <tr>
-                        <th> </th>
-                        <th> Acteur </th>
-                        <th> Rôle </th>
+                        <th></th>
+                        <th>Acteur</th>
+                        <th>Rôle</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php   
-                        foreach($requeteCast->fetchAll() as $detailCast) {?>
-                            <tr> 
-                                <td> <a href="index.php?action=detailActeur&id=<?= $detailCast["id_acteur"] ?>"><img style="border-radius: 15px; padding-bottom: 10px;width: 150px; height: 200px;" src='public/images/<?= $detailCast["affiche_acteur"] ?>' alt='Affiche du film'></a> </td>
-                                <td> <a href="index.php?action=detailActeur&id=<?= $detailCast["id_acteur"] ?>"><?= $detailCast["acteurName"] ?></a> </td> 
-                                <td> <a href="index.php?action=detailActeur&id=<?= $detailCast["id_role"] ?>"><?= $detailCast["nom_role"] ?> </a></td>
-                            </tr> 
-
+                    <?php foreach($requeteCast->fetchAll() as $detailCast) { ?>
+                        <tr> 
+                            <td>
+                                <a href="index.php?action=detailActeur&id=<?= $detailCast["id_acteur"] ?>">
+                                    <img src='public/images/<?= $detailCast["affiche_acteur"] ?>' alt='Affiche de l'acteur'>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="index.php?action=detailActeur&id=<?= $detailCast["id_acteur"] ?>"><?= $detailCast["acteurName"] ?></a>
+                            </td> 
+                            <td>
+                                <a href="index.php?action=detailActeur&id=<?= $detailCast["id_role"] ?>"><?= $detailCast["nom_role"] ?></a>
+                            </td>
+                        </tr> 
                     <?php } ?>
                 </tbody>
             </table>
         </div>
-
     </div>
-    <br>
-    
 </section>
 
 <?php
-
 $titre = "Détails Films";
 $titre_secondaire = "Détails Films";
 $contenu = ob_get_clean();
 require "view/template.php";
-
 ?>
